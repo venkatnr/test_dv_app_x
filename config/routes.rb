512e2e9f2a4c792cad13f692f07fd7636f6a3394
venkatnr <oneguyvenkat007@gmail.com>
2012-08-07 15:07:41 +0530

    modification for UI design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Xplanner::Application.routes.draw do
+
  match "/auth/:provider/callback" => "sessions#create"
  match "/signout" => "sessions#destroy", :as => :signout
+ 
   get "sessions/create"
-
   get "sessions/destroy"
 
   resources :projects do
