987f8e3fa61e604845b2e591cc9bd80e312e0470
venkatnr <oneguyvenkat007@gmail.com>
2012-08-07 18:01:30 +0530

    modification on model

--- app/models/usdemo.rb
+++ app/models/usdemo.rb
@@ -1,6 +1,6 @@
 class Usdemo < ActiveRecord::Base
  #belongs_to :project
   belongs_to :iteration
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   attr_accessible :customer, :description, :disposition, :estimated_hours, :name, :status, :tracker, :projectid
 end
