8573c94f1d177ff65ecc0824a7af7ed44b5158ff
venkatnr <oneguyvenkat007@gmail.com>
2012-08-07 17:36:50 +0530

    modification on iteration page

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -83,7 +83,7 @@ class IterationsController < ApplicationController
     @iteration.destroy
 
     respond_to do |format|
-      format.html { redirect_to iterations_url }
+      format.html { redirect_to project_iterations_url }
       format.json { head :no_content }
     end
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :iterations, :dependent => :destroy
+  has_many :iterations
 	belongs_to :user
   attr_accessible :description, :name, :user_id
 end
