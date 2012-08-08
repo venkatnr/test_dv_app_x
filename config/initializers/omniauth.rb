26fa3bf8c5925699d67e21e17d8172b654a4fdeb
venkatnr <oneguyvenkat007@gmail.com>
2012-08-08 12:04:03 +0530

    routes modification

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,9 @@
-OmniAuth.config.full_host = "  http://xplanner.herokuapp.com/"
+if Rails.env.production?
+  OmniAuth.config.full_host = "http://xplanner-name.heroku.com"
+else
+  OmniAuth.config.full_host = "http://localhost:3000"
+end
+#OmniAuth.config.full_host = "http://localhost:3000"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google, '1096749611269.apps.googleusercontent.com', '78C8VXtJfdbOohHlEFWOR6Uc', :scope => 'https://mail.google.com/mail/feed/atom/'
