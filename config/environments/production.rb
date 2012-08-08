fb5121a513d5e4e6eca27f8c359319b6107ec042
venkatnr <oneguyvenkat007@gmail.com>
2012-08-08 13:18:26 +0530

    first modification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,9 @@ Xplanner::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
+  config.assets.compile = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
